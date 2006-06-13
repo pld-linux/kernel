c0b22b730f6d987deb3a3f6106e480bf138ce6a3
sparky <sparky@pld-linux.org>
2006-06-13 21:59:06 +0000

    - more perl, less black magic
    Changed files:
        kernel-module-build.pl -> 1.1.2.2

--- kernel-module-build.pl
+++ kernel-module-build.pl
@@ -1,18 +1,38 @@
 #!/usr/bin/perl
+#
+use strict;
+use warnings;
+use File::Find qw(find);
 
-open( F, $ARGV[0]) or die("cannot open file: $ARGV[0]\n" );
-@lines = <F>;
-close( F );
+my $rpmdir = shift @ARGV or die;
+my $fileoutdir = shift @ARGV or die;
+my @tosort;
 
-foreach (@lines)
-{
-	@pe = split( '/', $_ );
-	my $tmp;
-	for my $p (0 .. $#pe - 1)
-	{
-		$tmp = $tmp . '/' . $pe[$p];
-		print( "%dir $ARGV[1]$tmp\n" );
+find(\&wanted, ".");
+
+sub wanted {
+	return unless -f;
+	return unless /^Kconfig/ or /^Makefile/;
+	#return if /\.orig$/;
+	return if $File::Find::name =~ /(Documentation|scripts)/;
+	(my $file = $File::Find::name) =~ s#^\./##;
+	$file =~ m#^(.*)/#;
+	my $dir = $1;
+	my $subdir = "";
+	foreach my $sub ( split( '/', $dir )) {
+		$subdir .= "/" . $sub;
+		push @tosort, "\%dir $rpmdir$subdir\n";
 	}
-	my $file = join( '/', @pe );
-	print( "$ARGV[1]/$file" );
+	push @tosort, "$rpmdir/$file\n";
 }
+
+my $last = "";
+my @toprint = grep {if ($_ ne $last) { $last = $_; 1} else {0}} sort @tosort;
+
+open F_OUT, "> $fileoutdir/aux_files" or die "Can't create aux_files: $!\n";
+print F_OUT @toprint;
+close F_OUT and print "aux_files created\n";
+
+open F_OUT, "> $fileoutdir/aux_files_exc" or die "Can't create aux_files_exc: $!\n";
+print F_OUT map {"\%exclude $_"} @toprint;
+close F_OUT and print "aux_files_exc created\n";
