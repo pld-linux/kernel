2380c4865cc3612d22b7c8f21890de152daf79a1
Jan Rękorajski <baggins@pld-linux.org>
2009-03-31 12:04:41 +0000

    - merged from LINUX_2_6
    Changed files:
        esfq-kernel.patch -> 1.2
        hostap-kernel-2.6.18.patch -> 1.2
        kernel-PF_RING.patch -> 1.2
        kernel-alpha.config -> 1.95
        kernel-apparmor-after-grsec_full.patch -> 1.2
        kernel-apparmor.patch -> 1.2
        kernel-atm-vbr.patch -> 1.3
        kernel-atmdd.patch -> 1.3
        kernel-aufs-support.patch -> 1.2
        kernel-aufs.patch -> 1.2
        kernel-autoconf.h -> 1.4
        kernel-bzip2-lzma.patch -> 1.2
        kernel-config.h -> 1.2
        kernel-esfq.patch -> 1.2
        kernel-fbcon-margins.patch -> 1.4
        kernel-fbcondecor.patch -> 1.2
        kernel-forcedeth-WON.patch -> 1.3
        kernel-grsec-caps.patch -> 1.2
        kernel-grsec-common.patch -> 1.2
        kernel-grsec-minimal.patch -> 1.3
        kernel-grsec-no-stupid-SbO.patch -> 1.2
        kernel-grsec.config -> 1.30
        kernel-grsec_fixes.patch -> 1.2
        kernel-grsec_full.patch -> 1.2
        kernel-grsec_minimal.config -> 1.2
        kernel-hostap.patch -> 1.2
        kernel-ia64.config -> 1.18
        kernel-imq.config -> 1.2
        kernel-imq.patch -> 1.2
        kernel-inittmpfs.config -> 1.2
        kernel-inittmpfs.patch -> 1.2
        kernel-ipt_ACCOUNT.patch -> 1.2
        kernel-ipt_account.patch -> 1.2
        kernel-ipvs-nfct.patch -> 1.2
        kernel-layer7.patch -> 1.2
        kernel-module-build.pl -> 1.2
        kernel-mpt-fusion.patch -> 1.2
        kernel-multiarch.config -> 1.2
        kernel-netfilter.config -> 1.45
        kernel-nf_rtsp.patch -> 1.3
        kernel-no-pax.config -> 1.2
        kernel-owner-xid.patch -> 1.2
        kernel-pax.config -> 1.2
        kernel-pax.patch -> 1.2
        kernel-pax_selinux_hooks.patch -> 1.2
        kernel-pom-ng-IPV4OPTSSTRIP.patch -> 1.2
        kernel-pom-ng-ROUTE.patch -> 1.2
        kernel-pom-ng-ipv4options.patch -> 1.2
        kernel-pom-ng-mms-conntrack-nat.patch -> 1.2
        kernel-pom-ng-rpc.patch -> 1.2
        kernel-pom-ng-rsh.patch -> 1.2
        kernel-powerpc.config -> 1.4
        kernel-ppc-ICE-hacks.patch -> 1.2
        kernel-ppc-crtsavres.patch -> 1.3
        kernel-ppclibs.Makefile -> 1.2
        kernel-pwc-uncompress.patch -> 1.2
        kernel-regressions.patch -> 1.3
        kernel-reiser4.patch -> 1.3
        kernel-rndis_host-wm5.patch -> 1.2
        kernel-routes.patch -> 1.2
        kernel-small_fixes.patch -> 1.2
        kernel-sparc.config -> 1.62
        kernel-sparc64.config -> 1.93
        kernel-suspend2-headers.patch -> 1.2
        kernel-tomoyo-with-apparmor.patch -> 1.2
        kernel-toshiba_acpi.patch -> 1.3
        kernel-tuxonice-headers.patch -> 1.2
        kernel-tuxonice.config -> 1.2
        kernel-tuxonice.patch -> 1.2
        kernel-unionfs-apparmor.patch -> 1.2
        kernel-unionfs.patch -> 1.2
        kernel-vserver-2.3.patch -> 1.3
        kernel-vserver-fixes.patch -> 1.2
        kernel-vserver.config -> 1.3
        kernel-warnings.patch -> 1.2
        kernel-wrr.patch -> 1.2
        kernel-x86.config -> 1.4
        kernel-zph.patch -> 1.2
        linux-2.6-atm-vbr.patch -> 1.3
        linux-2.6-atmdd.patch -> 1.3
        linux-2.6-forcedeth-WON.patch -> 1.3
        linux-2.6-grsec-caps.patch -> 1.2
        linux-2.6-grsec-common.patch -> 1.2
        linux-2.6-grsec-minimal.patch -> 1.3
        linux-2.6-grsec-no-stupid-SoB.patch -> 1.2
        linux-2.6-grsec_full.patch -> 1.2
        linux-2.6-ppc-ICE-hacks.patch -> 1.2
        linux-2.6-suspend2.patch -> 1.2
        linux-2.6-toshiba_acpi.patch -> 1.3
        linux-2.6-vs2.3.patch -> 1.3
        linux-2.6-warnings.patch -> 1.2
        linux-2.6.21.1-pwc-uncompress.patch -> 1.2
        linux-PF_RING.patch -> 1.2
        linux-fbcon-margins.patch -> 1.4
        pax_selinux_hooks-2.6.20.patch -> 1.2
        tahoe9xx-2.6.24.patch -> 1.2

--- kernel-autoconf.h
+++ kernel-autoconf.h
@@ -1,24 +1,2 @@
-#ifndef __pld_kernel_autoconf_h__
-#define __pld_kernel_autoconf_h__
-
-/*
- * Define some nasty macros o we can construct the file names
- * we want to include
- */
-
-#if defined(__pld_autoconf_included_file__)
-#undef __pld_autoconf_included_file__
-#endif /* __pld_autoconf_included_file__ */
-
-#if defined(__KERNEL_SMP)
-#include <linux/autoconf-smp.h>
-#define __pld_autoconf_included_file__ 1
-#endif /* __KERNEL_SMP */
-
-#if !defined(__pld_autoconf_included_file__)
-#include <linux/autoconf-up.h>
-#else
-#undef __pld_autoconf_included_file__
-#endif /* __pld_autoconf_included_file__ */
-
-#endif /* __pld_kernel_autoconf_h__ */
+#error Runtime kernel config detection not available.
+#error Please use the autoconf-dist.h instead.
